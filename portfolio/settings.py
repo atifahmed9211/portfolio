5cb53d88aa506e56cbc92bc6ee7e9c71615225c6
Atif Ahmed <atif.ahmed9211@gmail.com>
2021-04-06 19:49:09 +0500

    password updated in settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD' :'12345',
+        'PASSWORD' :'03350401095king',
         'HOST':'127.0.0.1', # 'HOST': 'localhost'
         'PORT':'5432',
 
